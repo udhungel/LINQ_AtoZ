288c4ab62003f42551694dacfdecb9605a6f5a25
udhungel <udeepdhungel@gmail.com>
2022-11-21 19:28:13 +0100

    Part 2 - Extension method In order to exten you need to make static and the parameter type need to proceed with this

--- Program.cs
+++ Program.cs
@@ -10,23 +10,22 @@ namespace LINQ_AtoZ
     public class Program
     {
         static void Main(string[] args)
-        {
-           IEnumerable<Student> stdResult =  Student.GetAllStudents().Where(s => s.Gender == "Male");
-            foreach (var stud in stdResult)
+        {          
+            string strName = "jordy";
+            string result = strName.ChangeFirstLetterCase();
+
+            Console.WriteLine("After using Extension method , Name is  {0} ", result);
+
+
+            Console.WriteLine("---------------------------xxxxx--------------------------------------xxxxxxx----------------------------- ");
+
+            //Other example using Where which is a extension method in IEnumerable<T>
+            // in order to exten you need to make static and the parameter type need to proceed with this
+            List<int> listnum= new List<int>() {1,2,3,4,5,6,7,8,9,10 };
+            IEnumerable<int> numbers = Enumerable.Where(listnum, n => n % 2 == 0);
+            foreach (int number in numbers) 
             {
-                Console.WriteLine("Student Name {0} amd his Id is {1} . Using lamda expression ", stud.Name, stud.ID);
-
-            }
-
-            Console.WriteLine("---------------------------------------------------------------------------------------------- ");
-
-            IEnumerable<Student> queryResult = from student in Student.GetAllStudents()
-                                               where student.Gender == "Male"
-                                               select student ;
-            foreach (var stud in stdResult)
-            {
-                Console.WriteLine("Student Name {0} amd his Id is {1} . Using  SQL Like query", stud.Name, stud.ID);
-
+                Console.WriteLine("Using Ienumberabl and using Where clause as extenstion while passing source {0} ", number);
             }
 
             Console.ReadLine();
