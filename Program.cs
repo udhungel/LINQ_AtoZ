ae2fd9b74a409167f9f518990bfe5a8b3a95af2b
udhungel <udeepdhungel@gmail.com>
2022-11-21 21:03:57 +0100

    Part 4 : Restriction Operators Added Entity Framework to the Console App Used Where clause to filter the Employe table

--- Program.cs
+++ Program.cs
@@ -11,45 +11,17 @@ namespace LINQ_AtoZ
     {
         static void Main(string[] args)
         {
-            //Linq Aggregate Functions
-            //MIN, MAX, SUM,COUNT,AVERAGE & AGGREGATE
+            EmployeeDBContext empdb = new EmployeeDBContext();
+            IEnumerable<tblEmployee> emplyee= empdb.tblEmployees.Where(x => x.Gender == "Male");
 
-            int[] Numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            foreach (var emp in emplyee)
+            {
+                Console.WriteLine("Employe Name {0} ", emp.Name);
 
-            //SmallestNumber
-            int smallestNumber = Numbers.Min();
-            int smallestEvenNumber = Numbers.Where(x => x % 2 == 0).Min(); 
+            }
 
-            //Largest Number 
-            int largestNumber = Numbers.Max();
-            int largetstEvenNumber = Numbers.Where(x=>x % 2 == 0).Max();
-
-            //Sum of all numbers 
-            int sumOfAllNumbers = Numbers.Sum(x => x);
-            int sumOfEvenNumbers = Numbers.Where(x=>x%2 == 0).Sum();
-
-            //Count of all Numbers
-            int countOfAllNumbers = Numbers.Count();
-            int countOfAllEvenNumbers = Numbers.Where(x => x % 2 == 0).Count();
-
-            //Average of all Numbers
-            double averageOfAllNumbers = Numbers.Average();
-            double averageOfAllEvenNumbers = Numbers.Where(x => x % 2 == 0).Average();
 
             
-            Console.WriteLine("---------------------------xxxxx--------------------------------------xxxxxxx----------------------------- ");
-            // Agregate function 
-            string[] countries = { "USA","CANADA","MEXICO" };
-
-            string result = countries.Aggregate((a, b) => a + "," + b);
-
-            Console.WriteLine(result);
-
-            int[] Num = { 1, 2, 3, 4, 5 };
-            int resultNum = Num.Aggregate((a,b) => a * b);
-
-
-            Console.WriteLine(resultNum);
             Console.ReadLine();
         }
 
