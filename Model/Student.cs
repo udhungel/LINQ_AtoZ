cadae20737c58265641f1f0dbac8c39999c9639e
udhungel <udeepdhungel@gmail.com>
2022-11-21 22:00:24 +0100

    Part 6 : SelectMany Operator Flattens the inner list Added Subject list in Student Used SelectMany to query different dataset and Student object

--- Model/Student.cs
+++ Model/Student.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,14 +13,16 @@ namespace LINQ_AtoZ.Model
         public string   Name { get; set; }
         public string Gender { get; set; }
 
+        public List<string> Subjects { get; set; }
+
         public static List<Student> GetAllStudents() 
         {
-            List<Student> listStudents = new List<Student>() { new Student { ID = 1 , Name = "Joe", Gender = "Male" },
-                                                               new Student { ID = 2 , Name = "Josh", Gender = "Male" },
-                                                               new Student { ID = 3 , Name = "Harry", Gender = "Male" },
-                                                               new Student { ID = 4 , Name = "Rose", Gender = "Female" },
-                                                               new Student { ID = 5 , Name = "Jasmin", Gender = "Female"},
-                                                               new Student { ID = 6 , Name = "Stephanie", Gender = "Female"}
+            List<Student> listStudents = new List<Student>() { new Student { ID = 1 , Name = "Joe", Gender = "Male", Subjects = new List<string>{"MVC", "JAVA", "SQL" } },
+                                                               new Student { ID = 2 , Name = "Josh", Gender = "Male", Subjects = new List<string>{"JAVA", "C#", "SQL" } },
+                                                               new Student { ID = 3 , Name = "Harry", Gender = "Male", Subjects = new List<string>{"JQUERY", "REACT", "SQL" } },
+                                                               new Student { ID = 4 , Name = "Rose", Gender = "Female" , Subjects = new List<string>{"REACT", "C#", "SQL" }},
+                                                               new Student { ID = 5 , Name = "Jasmin", Gender = "Female", Subjects = new List<string>{"JAVASCRIPT", "C++", "SQL" }},
+                                                               new Student { ID = 6 , Name = "Stephanie", Gender = "Female", Subjects = new List<string>{"C++", "C#", "SQL" }}
 
                                                              };
             return listStudents;
--- Program.cs
+++ Program.cs
@@ -1,9 +1,12 @@
 ﻿using LINQ_AtoZ.Model;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace LINQ_AtoZ
 {
@@ -11,20 +14,63 @@ namespace LINQ_AtoZ
     {
         static void Main(string[] args)
         {
-            EmployeeDBContext empdb = new EmployeeDBContext();
-            IEnumerable<string> emplyee= empdb.tblEmployees.Select(x => x.Gender); // this will only select that column gender 
+            IEnumerable<string> subjects = Student.GetAllStudents().SelectMany(s => s.Subjects).Distinct(); //single Ienumerable of string. flattens the inner Ienumerable which contains the list of all subjects of all students 
 
-            var employees = empdb.tblEmployees.Select(x => new {FirstName = x.Name , Gender = x.Gender , Department = x.departmentId });  // using anonymuous type 
-
-            foreach (var emp in emplyee)
+            foreach (var sub in subjects)
             {
-                Console.WriteLine("Employe Name : {0} ", emp);
+                Console.WriteLine(sub);
             }
 
-            foreach (var emp in employees)
+            Console.WriteLine("-------------------------Using Sql Like Query -------------------------------------");
+
+            //here we select student from the Getall Students 
+            //the second query will select the subject from the student query 
+            IEnumerable<string> subject = from student in Student.GetAllStudents()
+                                          from sub in student.Subjects
+                                          select sub;
+            foreach (var sub in subject)
             {
-                Console.WriteLine("Employe Name :  {0} , Gender : {1} , Department :  {2}  ", emp.FirstName , emp.Gender , emp.Department);
+                Console.WriteLine(sub);
             }
+            Console.WriteLine("-------------------------Another example to flatten alphabet into a single list -------------------------------------");
+
+
+            string[] stringArray = { "ABCDEFGHIJKLMNOPQRTUVSWXYZ", "0123456789" };
+
+            var result = stringArray.SelectMany(s => s);
+
+            //sql like 
+            var resultSQLLike = from s in stringArray
+                                from c in s
+                                select c;
+
+            foreach (char c in result)
+            {
+                Console.WriteLine(c);
+
+            }
+
+            Console.WriteLine("-------------------------resultSQLLike-------------------------------------");
+
+            foreach (char c in resultSQLLike)
+            {
+                Console.WriteLine(c);
+
+            }
+
+            Console.WriteLine("--------------Student with Subject -------------------------------- above example ----------------");
+
+            var subResult = Student.GetAllStudents().SelectMany(s=> s.Subjects,(student, Subject ) => new { StudentName = student.Name, subjectName = Subject });
+
+            var subResultSqlLike = from student in Student.GetAllStudents()
+                                   from sub in student.Subjects
+                                   select  new { StudentName = student.Name, subjectName = sub };
+
+            foreach (var sub in subResultSqlLike)
+            {
+                Console.WriteLine("StudentName " + sub.StudentName + "SubjectName :  "+ sub.subjectName);
+            }
+
 
             Console.ReadLine();
         }
