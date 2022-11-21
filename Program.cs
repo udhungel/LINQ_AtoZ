eb9b9417190e128e944f51961b7747845eda5269
udhungel <udeepdhungel@gmail.com>
2022-11-21 18:49:46 +0100

    Part 1 : Using Linq Query In-memory object

--- Program.cs
+++ Program.cs
@@ -11,7 +11,25 @@ namespace LINQ_AtoZ
     {
         static void Main(string[] args)
         {
-         
+           IEnumerable<Student> stdResult =  Student.GetAllStudents().Where(s => s.Gender == "Male");
+            foreach (var stud in stdResult)
+            {
+                Console.WriteLine("Student Name {0} amd his Id is {1} . Using lamda expression ", stud.Name, stud.ID);
+
+            }
+
+            Console.WriteLine("---------------------------------------------------------------------------------------------- ");
+
+            IEnumerable<Student> queryResult = from student in Student.GetAllStudents()
+                                               where student.Gender == "Male"
+                                               select student ;
+            foreach (var stud in stdResult)
+            {
+                Console.WriteLine("Student Name {0} amd his Id is {1} . Using  SQL Like query", stud.Name, stud.ID);
+
+            }
+
+            Console.ReadLine();
         }
 
         
